df924314c1c8f2c92680115f76dbb6d957ad311a
Agustin <agustin.derobles1995@gmail.com>
2024-08-26 21:33:01 -0300

    fix vercel error

--- email-frontend/vite.config.js
+++ email-frontend/vite.config.js
@@ -1,9 +1,10 @@
+// email-frontend/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist' // Asegúrate de que esto esté configurado
   }
 });
